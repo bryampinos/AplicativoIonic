a2aa7adb53e45fd0e6c47c6f819f0490d4ee52ff
kevin <kevin.chumbay.07@est.ucacue.edu.ec>
2024-06-04 09:18:23 -0500

    para desplegar

--- src/Component/ScannerQR/scanner-qr/scanner-qr.component.ts
+++ src/Component/ScannerQR/scanner-qr/scanner-qr.component.ts
@@ -1,79 +1,46 @@
-import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
-import jsQR from 'jsqr';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import { BrowserMultiFormatReader, Result, NotFoundException } from '@zxing/library';
 
 @Component({
   selector: 'app-scanner-qr',
   templateUrl: './scanner-qr.component.html',
   styleUrls: ['./scanner-qr.component.scss'],
 })
-export class ScannerQRComponent implements AfterViewInit {
-  @ViewChild('videoElement') videoElement!: ElementRef<HTMLVideoElement>;
-  @ViewChild('canvasElement') canvasElement!: ElementRef<HTMLCanvasElement>;
-  qrCodeData: string | null = null;
-
-  constructor() {}
-
-  ngAfterViewInit() {
-    this.startVideo();
-  }
-
-  async startVideo() {
-    try {
-      const constraints = {
-        video: {
-          facingMode: 'environment',
-        },
-      };
-
-      const stream = await navigator.mediaDevices.getUserMedia(constraints);
-      const video = this.videoElement.nativeElement;
-
-      if (video) {
-        video.srcObject = stream;
-        video.setAttribute('playsinline', ''); // Necessary for iOS
-
-        await new Promise<void>((resolve, reject) => {
-          video.onloadedmetadata = () => {
-            resolve(video.play());
-          };
-          video.onerror = (error) => {
-            reject(error);
-          };
-        });
-
-        this.scanQRCode();
+export class ScannerQRComponent implements AfterViewInit, OnDestroy{
+    @ViewChild('video', { static: false }) video: ElementRef<HTMLVideoElement> | undefined;
+  
+    private codeReader: BrowserMultiFormatReader;
+    public qrResult: string;
+  
+    constructor() {
+      this.codeReader = new BrowserMultiFormatReader();
+      this.qrResult = '';
+    }
+  
+    ngAfterViewInit(): void {
+      if (this.video) {
+        this.startScan();
       }
-    } catch (error) {
-      console.error('Error accessing the camera: ', error);
-      alert('Error accessing the camera: ' + error);
+    }
+  
+    startScan(): void {
+      if (!this.video) {
+        console.error('Video element not found!');
+        return;
+      }
+  
+      this.codeReader.decodeFromVideoDevice(null, this.video.nativeElement, (result: Result | null, error: any) => {
+        if (result) {
+          this.qrResult = result.getText();
+          alert(`QR Code Data: ${this.qrResult}`);
+        }
+        if (error && !(error instanceof NotFoundException)) {
+          console.error('Error scanning QR code', error);
+        }
+      });
+    }
+  
+    ngOnDestroy(): void {
+      this.codeReader.reset();
     }
   }
-
-  scanQRCode() {
-    const canvas = this.canvasElement.nativeElement;
-    const context = canvas.getContext('2d');
-    const video = this.videoElement.nativeElement;
-
-    const tick = () => {
-      if (video.readyState === video.HAVE_ENOUGH_DATA && context) {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        context.drawImage(video, 0, 0, canvas.width, canvas.height);
-        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
-        const code = jsQR(imageData.data, imageData.width, imageData.height);
-
-        if (code) {
-          this.qrCodeData = code.data;
-          const tracks = (video.srcObject as MediaStream).getTracks();
-          tracks.forEach(track => track.stop());
-        }
-      }
-
-      if (!this.qrCodeData) {
-        requestAnimationFrame(tick);
-      }
-    };
-
-    tick();
-  }
-}
